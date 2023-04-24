7204fe76e14e40719cd8855a4296767d75ff3d98
liguohan <2282224584@qq.com>
2023-04-24 20:49:23 +0800

    feat: 添加页面标题

---
+++ src/components/FirewiseTitle.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="title-bg">
+    <span class="title">智慧消防</span>
+    <span class="sub-title">可视化分析系统</span>
+  </div>
+
+  <div class="location-bg">
+    <span class="country">中国</span>
+    <span class="city">杭州市</span>
+  </div>
+</template>
+
+<style scoped>
+.title-bg {
+  position: absolute;
+  width: 208px;
+  height: 299px;
+  left: -42px;
+  top: 363px;
+
+  background: linear-gradient(267.18deg, rgba(66, 74, 113, 0.7) 2.51%, rgba(66, 74, 113, 0) 65.21%);
+  border-radius: 41px;
+}
+
+.title {
+  /* 智慧消防 */
+
+  position: absolute;
+  width: 29px;
+  height: 265px;
+  left: 94px;
+  top: 34px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 45px;
+  line-height: 53px;
+  text-align: center;
+
+  color: #ffffff;
+}
+
+.sub-title {
+  /* 可视化分析系统 */
+
+  position: absolute;
+  width: 29px;
+  height: 196px;
+  left: 144px;
+  top: 38px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 28px;
+  text-align: center;
+
+  color: #a8a6f7;
+}
+
+.location-bg {
+  position: absolute;
+  width: 166px;
+  height: 102px;
+  left: 0px;
+  top: 683px;
+
+  background: linear-gradient(267.18deg, rgba(66, 74, 113, 0.7) 2.51%, rgba(66, 74, 113, 0) 65.21%);
+  border-radius: 41px;
+}
+
+.country {
+  /* 中国 */
+
+  position: absolute;
+  width: 82px;
+  height: 23px;
+  left: 42px;
+  top: 15px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 23px;
+  text-align: center;
+
+  color: #968adc;
+}
+
+.city {
+  /* 杭州市 */
+
+  position: absolute;
+  width: 82px;
+  height: 29px;
+  left: 44px;
+  top: 52px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 29px;
+  text-align: center;
+
+  color: #ffffff;
+}
+</style>
